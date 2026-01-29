126fca2cc1ba2502f7cfa2740a6b814ae376596e
rfernandez <rfernandez4@apple.com>
2026-01-29 22:40:14 +0100

    custom

--- install/config/hardware/fix-asus-rog-audio-mixer.sh
+++
@@ -1,9 +0,0 @@
-# Fix audio volume on Asus ROG laptops by using a soft mixer.
-
-if [[ "$(cat /sys/class/dmi/id/sys_vendor 2>/dev/null)" == "ASUSTeK COMPUTER INC." ]] &&
-   grep -q "ROG" /sys/class/dmi/id/product_family 2>/dev/null; then
-
-  mkdir -p ~/.config/wireplumber/wireplumber.conf.d/
-  cp $OMARCHY_PATH/default/wireplumber/wireplumber.conf.d/alsa-soft-mixer.conf ~/.config/wireplumber/wireplumber.conf.d/
-  rm -rf ~/.local/state/wireplumber/default-routes
-fi
